a2899388128ce73b5e5d9cfee7bc893bfc938a46
Pogokoala <pogo_33125@yahoo.com>
2022-10-06 00:56:04 +0000

    update some code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The guinea pig", "Chubbie", "His Pet", "My pig"];
+  let action = ["ate", "groge", "popcorned", "zoomed"];
+  let what = ["the pallet", "a carrot", "some cucumbers"];
+  let when = ["yesterday.", "last week.", "2 months ago."];
+
+  function guineaPig() {
+    document.getElementById("Pig").innerHTML =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+  }
+  document.getElementById("btn").addEventListener("click", guineaPig);
 };
